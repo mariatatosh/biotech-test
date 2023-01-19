680b6b651f2b3083e9ac538be885cbf2ad76c8b2
Maria Tatosh <tatoshmaria@protonmail.com>
2023-01-19 22:49:03 +0300

    Create payment details form and fix file naming

---
+++ app/resources/views/home.blade.php
@@ -0,0 +1,75 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Payment Details') }}</div>
+
+                    <div class="card-body">
+                        <form method="POST" action="#">
+                            @csrf
+
+                            <div class="row mb-3">
+                                <label for="name"
+                                       class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+
+                                <div class="col-md-6">
+                                    <input
+                                        id="name"
+                                        type="text"
+                                        class="form-control"
+                                        name="name"
+                                        autocomplete="name"
+                                    >
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <label for="phone"
+                                       class="col-md-4 col-form-label text-md-end">{{ __('Phone Number') }}</label>
+
+                                <div class="col-md-6">
+                                    <input
+                                        id="phone"
+                                        type="text"
+                                        class="form-control"
+                                        name="phone"
+                                        autocomplete="tel"
+                                        required
+                                    >
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <label for="email"
+                                       class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+
+                                <div class="col-md-6">
+                                    <input
+                                        id="email"
+                                        type="email"
+                                        class="form-control"
+                                        name="email"
+                                        autocomplete="email"
+                                        required
+                                        autofocus
+                                    >
+                                </div>
+                            </div>
+
+                            <div class="row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Pay $243') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
